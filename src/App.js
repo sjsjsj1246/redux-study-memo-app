171c43770e0caec4f28bd963163051051c560b67
Inseo Hwang <sjsjsj1246@gmail.com>
2021-09-01 03:02:20 +0900

    Feat: redux, pender, thunk

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
 import * as counterActions from './modules/counter';
+import * as postActions from './modules/post';
 
 
 class App extends Component {
-    render() {
-        const { CounterActions, number } = this.props;
 
-        
+    componentDidMount() {
+        const { number } = this.props;
+        this.getPost(number);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(this.props.number !== nextProps.number) {
+            this.getPost(nextProps.number);
+        }
+    }
+
+    getPost = async (postId) => {
+        const { PostActions } = this.props;
+
+        try {
+            await PostActions.getPost(postId);
+            console.log('요청이 완료 된 다음에 실행됨')
+        } catch(e) {
+            console.log('에러가 발생!');
+        }
+    }
+
+    render() {
+        const { CounterActions, number, post, error, loading } = this.props;
+
         return (
             <div>
-                <h1>{number}</h1>
+                <p>{number}</p>
                 <button onClick={CounterActions.increment}>+</button>
                 <button onClick={CounterActions.decrement}>-</button>
+                { loading && <h2>로딩중...</h2>}
+                { error 
+                    ? <h1>에러발생!</h1> 
+                    : (
+                        <div>
+                            <h1>{post.title}</h1>
+                            <p>{post.title}</p>
+                        </div>
+                    )}
             </div>
         );
     }
@@ -21,9 +54,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.post,
+        loading: state.pender.pending['GET_POST'],
+        error: state.pender.failure['GET_POST']
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
--- src/store.js
+++ src/store.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 
-const store = createStore(modules)
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+import penderMiddleware from 'redux-pender';
+
+
+/* 로그 미들웨어를 생성 할 때 설정을 커스터마이징 할 수 있습니다.
+   https://github.com/evgenyrodionov/redux-logger#options
+*/
+const logger = createLogger(); 
+
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk, penderMiddleware()));
 
 export default store;
